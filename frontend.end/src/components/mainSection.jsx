7523d3615d7f648ec24f2b57ab213bd6c349fb68
Ishita282 <little.mini.ishita@gmail.com>
2025-02-15 22:56:56 +0530

    recommit inital files

--- frontend.end/src/components/mainSection.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-const mainSection = () => {
-  const [count, setCount] = useState();
-  return <div>Count: {count}</div>;
-};
-
-export default mainSection;
--- frontend.end/src/components/quizLists.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const quizLists = () => {
-  return <div></div>;
-};
-
-export default quizLists;
